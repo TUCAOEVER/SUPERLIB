1657a583e0bc553d9df4579adb33ab962d886bde
tucaoever <tucaofeng@sina.com>
2023-08-25 15:55:39 +0800

    临时推送 - 移除GUI相关注册

--- src/main/java/com/tucaoever/superlib/register/Conditions.java
+++ src/main/java/com/tucaoever/superlib/register/Conditions.java
@@ -11,22 +11,6 @@ import com.tucaoever.superlib.elements.others.scoreboard.conditions.CondIsScoreb
 
 public class Conditions {
     public static void register() {
-        Skript.registerCondition(SecMakeGUI.class,
-                "(make|format) [the] next gui [slot] (with|to) [(1¦([re]mov[e]able|stealable))] %itemtype%",
-                "(make|format) gui [slot[s]] %strings/numbers% (with|to) [(1¦([re]mov[e]able|stealable))] %itemtype%",
-                "(un(make|format)|remove) [the] next gui [slot]",
-                "(un(make|format)|remove) gui [slot[s]] %strings/numbers%",
-                "(un(make|format)|remove) all [of the] gui [slots]");
-        Skript.registerCondition(SecCreateGUI.class,
-                "create [a] [new] gui [[with id[entifier]] %-string%] with %inventory% [(1¦(and|with) (moveable|stealable) items)] [(and|with) shape %-strings%]",
-                "(change|edit) [gui] %guiinventory%");
-        Skript.registerCondition(SecOnCloseGUI.class,
-                "run (when|while) clos(e|ing) [[the] gui]",
-                "run (when|while) [the] gui closes",
-                "run on gui clos(e|ing)");
-        Skript.registerCondition(CondHasGUI.class,
-                "%players% (has|have) a gui [open]",
-                "%players% (doesn't|does not|do not|don't) have a gui [open]");
         Skript.registerCondition(CondFileExists.class,
                 "(file|folder) %string% exist[s]",
                 "(file|folder) %string% (does not|doesn't|is not|isn't) exist[s]");
